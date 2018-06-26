d1249dc0b4ba46b424af1091800f51829a11f0ab
austin <me@austbot.com>
2018-06-26 06:27:29 -0700

    fix aot build

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,21 +1,19 @@
 import {
-  ActionReducer,
   ActionReducerMap,
-  createFeatureSelector,
-  createSelector,
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
-import { StatsState } from '../statslist/state/stats-list.reducer';
 
 export interface State {
   app: {};
 }
 
+export function app(state, action) {
+  return state;
+}
+
 export const reducers: ActionReducerMap<State> = {
-  app: function (state, action) {
-    return state;
-  }
+  app
 };
 
 
